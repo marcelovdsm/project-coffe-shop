142b83d7343c47fd9bfdcc685236c3a451b8e0a5
Marcelo Victor <marcelovictorsm077@gmail.com>
2023-05-11 14:37:45 -0300

    Theme provider & Routes

--- coffe-shop/src/App.tsx
+++ coffe-shop/src/App.tsx
@@ -1,12 +1,19 @@
-import { ThemeProvider } from "styled-components"
-import { defaultTheme } from "./styles/themes/default"
-import { Header } from "./components/Header"
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Header } from './components/Header'
+import { Router } from './Router'
 
 export function App() {
-  return(
-  <ThemeProvider theme={defaultTheme}>
-    <Header />
-  </ThemeProvider>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Header />
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
--- coffe-shop/src/Router.tsx
+++ coffe-shop/src/Router.tsx
@@ -1,7 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './Pages/Home'
 import { Cart } from './Pages/Cart'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
 
 export const Router = () => {
-  return 
+  return (
+    <ThemeProvider theme={defaultTheme}>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
+    </ThemeProvider>
+  )
 }
